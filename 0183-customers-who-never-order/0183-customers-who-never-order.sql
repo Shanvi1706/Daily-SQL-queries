78378b0e174f97a3569940ff0834bfcd2bec8ec7
Shanvi <sanvi6398@gmail.com>
2025-08-30 20:14:52 +0530

    Time: 797 ms (8.88%), Space: 0B (100%) - LeetHub

---
+++ 0183-customers-who-never-order/0183-customers-who-never-order.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+SELECT c.`name` AS `Customers`
+FROM `Customers` AS c
+WHERE NOT EXISTS (
+  SELECT 1
+  FROM `Orders` AS o
+  WHERE o.`customerId` = c.`id`
+);
