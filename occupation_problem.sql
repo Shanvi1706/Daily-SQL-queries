6c8c7d0e4887d0395b7a9581113989405ecb25e8
Shanvi <sanvi6398@gmail.com>
2026-02-07 23:39:40 +0530

    Create occupation_problem.sql

---
+++ occupation_problem.sql
@@ -0,0 +1,18 @@
+-- Hackerrank : Occupation problem 
+-- Link : https://www.hackerrank.com/challenges/occupations/problem?isFullScreen=true
+
+--Solution:
+SELECT 
+    MAX(CASE WHEN Occupation = 'Doctor' THEN Name END) AS Doctor,
+    MAX(CASE WHEN Occupation = 'Professor' THEN Name END) AS Professor,
+    MAX(CASE WHEN Occupation = 'Singer' THEN Name END) AS Singer,
+    MAX(CASE WHEN Occupation = 'Actor' THEN Name END) AS Actor
+FROM (
+    SELECT 
+        Name,
+        Occupation,
+        ROW_NUMBER() OVER (PARTITION BY Occupation ORDER BY Name) AS rn
+    FROM OCCUPATIONS
+) t
+GROUP BY rn
+ORDER BY rn;
