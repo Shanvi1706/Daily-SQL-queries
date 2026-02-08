a14ca9a93f73d5a49f254fb010d3655d648c560d
Shanvi <sanvi6398@gmail.com>
2026-02-08 23:35:41 +0530

    Create bst.sql

---
+++ bst.sql
@@ -0,0 +1,13 @@
+--Hackerrank - Binary tree problem
+--Link - https://www.hackerrank.com/challenges/binary-search-tree-1/problem?isFullScreen=true
+
+Solution:
+SELECT 
+    N,
+    CASE
+        WHEN P IS NULL THEN 'Root'
+        WHEN N NOT IN (SELECT DISTINCT P FROM BST WHERE P IS NOT NULL) THEN 'Leaf'
+        ELSE 'Inner'
+    END AS NodeType
+FROM BST
+ORDER BY N;
