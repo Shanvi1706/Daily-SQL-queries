49a204ca08ca0e7ef6b8a8b79105c056a1c2f275
Shanvi <sanvi6398@gmail.com>
2026-02-11 22:25:26 +0530

    Create revising_aggregations.sql

---
+++ revising_aggregations.sql
@@ -0,0 +1,5 @@
+--Hackerrank - https://www.hackerrank.com/challenges/revising-aggregations-the-count-function/problem?isFullScreen=true
+--Solution - 
+SELECT COUNT(*)  
+FROM CITY
+WHERE POPULATION > 100000;
