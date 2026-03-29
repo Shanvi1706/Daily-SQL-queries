3d1bce82c84335aa5aeb946bf05ba40dc1eaeaa7
Shanvi <sanvi6398@gmail.com>
2026-03-29 23:56:33 +0530

    Create laptopvsmobileviewership.sql

---
+++ laptopvsmobileviewership.sql
@@ -0,0 +1,6 @@
+-- Datalemur - https://datalemur.com/questions/laptop-mobile-viewership
+-- Solution -
+SELECT
+    SUM(CASE WHEN device_type = 'laptop' THEN 1 ELSE 0 END) AS laptop_views,
+    SUM(CASE WHEN device_type IN ('tablet', 'phone') THEN 1 ELSE 0 END) AS mobile_views
+FROM viewership;
